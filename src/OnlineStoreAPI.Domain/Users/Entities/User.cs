78fb49388e3823b394ea33ec99318dbd0096498e
JAnMorss <johnanthonymorales124@gmail.com>
2025-06-13 22:11:02 +0800

    I change the user entity

--- src/OnlineStoreAPI.Domain/Users/Entities/User.cs
+++ src/OnlineStoreAPI.Domain/Users/Entities/User.cs
@@ -1,6 +1,9 @@
-﻿using OnlineStoreAPI.Domain.Users.Events;
+﻿using OnlineStoreAPI.Domain.Users.Errors;
+using OnlineStoreAPI.Domain.Users.Events;
+using OnlineStoreAPI.Domain.Users.Profiles;
 using OnlineStoreAPI.Domain.Users.ValueObjects;
 using OnlineStoreAPI.Shared.Kernel.Domain;
+using OnlineStoreAPI.Shared.Kernel.ErrorHandling;
 
 namespace OnlineStoreAPI.Domain.Users.Entities
 {
@@ -8,66 +11,96 @@ namespace OnlineStoreAPI.Domain.Users.Entities
     {
         private User() { }
 
-        public User(
+        private User(
             Guid id,
-            FullName fullName,
-            Email email,
             UserName userName,
-            PasswordHash passwordHash,
+            Email email,
             Role role) : base(id)
         {
-            FullName = fullName;
-            Email = email;
             UserName = userName;
-            PasswordHash = passwordHash;
+            Email = email;
             Role = role;
         }
 
-        public FullName FullName { get; private set; }
-
-        public Email Email { get; private set; }
-
         public UserName UserName { get; private set; }
-
-        public PasswordHash PasswordHash { get; private set; }
-
+        public Email Email { get; private set; }
         public Role Role { get; private set; }
 
-        public string IdentityId { get; private set; } = string.Empty;
+        public CustomerProfile? CustomerProfile { get; private set; }
+        public SellerProfile? SellerProfile { get; private set; }
 
-        public static User Create(
-            FullName fullName,
-            Email email,
+
+        public static Result<User> Create(
             UserName userName,
-            PasswordHash passwordHash,
+            Email email,
             Role role)
         {
             var user = new User(
                 Guid.NewGuid(),
-                fullName,
-                email,
                 userName,
-                passwordHash,
+                email,
                 role);
 
             user.RaiseDomainEvent(new UserCreatedDomainEvent(user.Id, user.Email.Value));
 
-            return user;
+            return Result.Success(user);
         }
 
-        public void SetIdentityId(string identityId)
+        public Result AddCustomerProfile(
+            FirstName firstName,
+            LastName lastName,
+            PhoneNumber phone,
+            Address address)
         {
-            IdentityId = identityId;
+            if (CustomerProfile is not null)
+            { 
+                return Result.Failure(UserErrors.CustomerProfileExists); 
+            }
+
+            CustomerProfile = new CustomerProfile(
+                firstName, 
+                lastName, 
+                phone, 
+                address);
+
+            return Result.Success();
         }
 
-        public bool IsInRole(Role role) => Role.Is(role);
-
-        public void ChangeRole(Role newRole)
+        public Result AddSellerProfile(
+            ShopName shopName,
+            StoreDescription description,
+            PhoneNumber phone,
+            Address address)
         {
+            if (SellerProfile is not null)
+            {
+                return Result.Failure(UserErrors.SellerProfileExists);
+            }
+
+            SellerProfile = new SellerProfile(
+                shopName, 
+                description, 
+                phone, 
+                address);
+
+            return Result.Success();
+        }
+
+        public Result ChangeRole(Role newRole)
+        {
+            if (Role.Is(newRole))
+            { 
+                return Result.Failure(UserErrors.UserSameRole); 
+            }
+
             Role = newRole;
 
             RaiseDomainEvent(new UserRoleChangedDomainEvent(Id, Role.Value));
 
+            return Result.Success();
         }
+
+        public bool IsInRole(Role role) => Role.Is(role);
     }
 }
+
