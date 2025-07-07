dd7b1341e6b7b1cea610bb9baaa0194733f8a2c0
JAnMorss <johnanthonymorales124@gmail.com>
2025-07-07 22:07:13 +0800

    feat: update UserRepository and related User entity interfaces

---
+++ src/OnlineStore.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStoreAPI.Domain.Users.Entities;
+using OnlineStoreAPI.Domain.Users.Interface;
+
+namespace OnlineStore.Infrastructure.Repositories
+{
+    internal sealed class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default)
+        {
+            return await _context.Users
+               .FirstOrDefaultAsync(u => u.Email.Value == email, cancellationToken);
+        }
+    }
+}
---
+++ src/OnlineStoreAPI.Domain/Users/Interface/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using OnlineStoreAPI.Domain.Users.Entities;
+
+namespace OnlineStoreAPI.Domain.Users.Interface
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+       
+        Task AddAsync(User user, CancellationToken cancellationToken = default);
+        Task UpdateAsync(User user, CancellationToken cancellationToken = default);
+        Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
