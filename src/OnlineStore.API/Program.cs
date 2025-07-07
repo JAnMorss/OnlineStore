7a98a881859e7c0c456bdcb4d6f516861fa3ef20
JAnMorss <johnanthonymorales124@gmail.com>
2025-07-07 22:07:26 +0800

    chore: add .dockerignore and initial OnlineStore.API structure

---
+++ src/OnlineStore.API/Program.cs
@@ -0,0 +1,28 @@
+using OnlineStore.Application;
+using OnlineStore.Infrastructure;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapScalarApiReference();
+    app.MapOpenApi();
+
+    //app.ApplyMigrations();
+
+    //app.SeedData();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
